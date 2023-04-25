2245f60428ff3207e74e46ea59276544db059f6e
parthsawant2001 <parthpsawant2001@gmail.com>
2023-04-25 15:51:20 +0530

    first commit

--- index.js
+++ index.js
@@ -76,17 +76,16 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
   const newPath = path + '.' + ext;
   fs.renameSync(path, newPath);
 
-  const { token } = req.cookies.token;
-  // const { username, password } = req.body;
-  // const passOk = bcrypt.compareSync(password, userDoc.password);
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  const { token } = jwt.sign(
+    { username, id: userDoc._id },
+    secret,
+    {},
+    (err, token) => {}
+  );
 
-  // jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-  //   if (err) throw err;
-  //   res.cookie('token', token).json({
-  //     id: userDoc._id,
-  //     username,
-  //   });
-  // });
   console.log(token);
   jwt.verify(token, secret, {}, async (err, info) => {
     if (err) throw err;
