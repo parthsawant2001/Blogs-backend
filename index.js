d999ff703a48bc298921b21e239e8cc2abced370
parthsawant2001 <parthpsawant2001@gmail.com>
2023-04-25 14:59:11 +0530

    first commit

--- index.js
+++ index.js
@@ -82,6 +82,14 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
   fs.renameSync(path, newPath);
 
   const { token } = req.cookies;
+  console.log(token);
+  // jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+  //   if (err) throw err;
+  //   res.cookie('token', token).json({
+  //     id: userDoc._id,
+  //     username,
+  //   });
+  // });
   jwt.verify(token, secret, {}, async (err, info) => {
     if (err) throw err;
     const { title, summary, content } = req.body;
